57dca2fbacec81029a8f778bbedb78b19ae244bf
zhushanbao <zhushanbao@bytedance.com>
2023-04-07 14:11:44 +0800

    update layout

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,54 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import ActivityList from 'components/ActivityList';
+import { MenuOutlined } from '@ant-design/icons';
 import './App.css';
+import Functional from 'components/Functional';
+import Content from 'components/Content';
 
-function Hello() {
+function App() {
+  const activities = [
+    {
+      key: '1',
+      label: <MenuOutlined />,
+      children: 'TEST ACTIVITY',
+    },
+  ];
+
+  const contents = [
+    {
+      key: '1',
+      label: 'Content',
+      children: 'TEST Content',
+    },
+  ];
+
+  const functionals = [
+    {
+      key: '1',
+      label: 'Logs',
+      children: 'TEST LOG',
+    },
+  ];
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+    <div className="container">
+      <div className="top" />
+      <div className="middle">
+        <div className="middle-left">
+          <ActivityList activities={activities} />
+        </div>
+        <div className="middle-center">
+          <div className="middle-center-upper">
+            <Content contents={contents} />
+          </div>
+          <div className="middle-center-lower">
+            <Functional functionals={functionals} />
+          </div>
+        </div>
+        <div className="middle-right" />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <div className="bottom" />
     </div>
   );
 }
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
-}
+export default App;
